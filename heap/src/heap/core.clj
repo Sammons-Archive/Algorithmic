7b922d1ef474dab02693fe4107bf51ffd6cd153e
Sammons <bsammnz@gmail.com>
2013-10-22 08:27:28 -0500

    changed some things at the RoR meeting

--- heap/src/heap/core.clj
+++ heap/src/heap/core.clj
@@ -1,17 +1,17 @@
 (ns heap.core (:gen-class))
 
-(def input-pattern  #"(?m)^[0-9]|^I\s[0-9]+|E|^C\s[0-9]+\s[0-9]+|^D\s[0-9]+");get over it, regex is so much fun
-
+;(def input-pattern  #"(?m)^[0-9]|^I\s[0-9]+|E|^C\s[0-9]+\s[0-9]+|^D\s[0-9]+");get over it, regex is so much fun
+(def input-pattern  #"^[0-9]$");
 (defn left 
 "get my wrong child"
-[i] (+ (* 2 i) 1))
+[i] (* 2 i))
 
 (defn right 
 "get my right child"
-[i] (+ (* 2 i) 2))
+[i] (+ (* 2 i) 1))
 
 (defn get-greatest [slice & us]
-	 apply max (map slice (apply vector us))
+	 (apply max (map slice (apply vector us)))
 	)
 (defn parent 
 "get my parent, you choose which"
@@ -19,18 +19,21 @@
 
 
 (defn swap [slice one two] (assoc slice two (slice one) one (slice two)))
-(defn max-heapify [slice & me]
-	(let []
-	))
+
+
+(defn max-heapify [slice me]
+	println slice me)
 
 (defn -main
 "heap operations"
 [& args]
 	print "what"
 	(let [
-		numbers  (apply vector 
-			(map #(. Integer parseInt %) 
-				(filter #(re-matches #"[0-9]+" %) 
-					 (re-seq input-pattern  (slurp "in.txt")))))
+		numbers (apply vector 
+		( cons 0  
+			(apply vector 
+				(map #(. Integer parseInt %) (re-seq #"(?m)^[0-9]" (slurp "in.txt"))))))
+		heap (map max-heapify 'numbers  numbers)
 		]
-		(max-heapify numbers 0)))
+		(max-heapify numbers 0)
+		numbers))
--- heap/test/heap/core_test.clj
+++ heap/test/heap/core_test.clj
@@ -2,9 +2,22 @@
   (:require [clojure.test :refer :all]
             [heap.core :refer :all]))
 
+
 (deftest test-greatest-finder
-  (= (get-greatest [0 1 2] 0 1) 1))
+  (is (= (get-greatest [0 1 2] 0 1) 1)))
+
 (deftest test-regex
-	(= (apply vector (map #(. Integer parseInt %) (filter #(re-matches #"[0-9]+" %) (re-seq input-pattern "0 1 2 4 E"))))
-		[0 1 2 4])
-	)
+	(let [ nums (apply vector 
+		( cons 0  
+			(apply vector 
+				(map #(. Integer parseInt %) (re-seq #"(?m)^[0-9]" "1\n2\n3\n4\nE\nE")))))
+		]
+		(is (= nums [0 1 2 3 4]))
+	))
+
+(deftest test-file (is (not= (slurp "in.txt") nil)))
+
+(deftest test-swap (is (= (swap [1 2 3] 2 1) [1 3 2])))
+
+;(deftest test-main (= (-main "in.txt") ()))
+
