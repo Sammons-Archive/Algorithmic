3213d643f0f9b82633d4957fef61d79f141e419c
Sammons <bsammnz@gmail.com>
2013-10-23 22:51:50 -0500

    retiring the heap in leu of brighter futures

--- heap/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject heap "0.1.0-SNAPSHOT"
-  :description "heap fun"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot heap.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
